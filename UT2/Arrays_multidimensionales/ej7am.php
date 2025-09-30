ef54235afb199ca4344e18cc59b5f8f63187fd6e
Miguel Durán <migueld202035@gmail.com>
2025-09-30 12:41:20 +0200

    UT2 Arrays multidimiensionales Ejercicio 7 | Array comenzado

--- UT2/Arrays_multidimensionales/ej7am.php
+++ UT2/Arrays_multidimensionales/ej7am.php
@@ -6,6 +6,14 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+        $matriz_clase = array(
+            "DWES" => [10,6,3,5,2,7,2,9,0,3],
+            "DWEC" => [4,3,6,7,1,2,1,0,8,10], 
+            "DIW"  => [5,6,7,8,3,4,1,2,9,6], 
+            "PEP"  => [7,4,3,7,10,0,5,4,7,8]);
+        
+        $echo "a: "
+    ?>
 </body>
 </html>
