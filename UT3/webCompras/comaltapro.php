e033c367fa1035bd123c0a74f1cb8ecbee758f8f
Miguel Durán <migueld202035@gmail.com>
2025-11-19 09:24:04 +0100

    UT3 Web Compras | Ej2 Terminado

--- UT3/webCompras/comaltapro.php
+++ UT3/webCompras/comaltapro.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     
-    <form action="./comaltapro2.php" method="post">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
         
         <p>Nombre del producto: </p>
         <input type="text" name="nombre_producto">
@@ -53,3 +53,74 @@
 </body>
 </html>
 
+<?php 
+
+    $nombre_producto = $precio_producto = $categoria_producto = "";
+    
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+        $nombre_producto = test_input($_POST['nombre_producto']);
+        $precio_producto = test_input($_POST['precio_producto']);
+        $categoria_producto = test_input($_POST['categoria_producto']);
+
+        try {
+
+            // Buscar la ID de la categoria segun el nombre
+
+            $stmt = $conn -> prepare("SELECT id_categoria FROM categoria WHERE nombre = '$categoria_producto'");
+            $stmt -> execute();
+            $stmt ->setFetchMode(PDO::FETCH_ASSOC);
+            $id_categoria = $stmt ->fetchAll(); 
+
+            $id_categoria = $id_categoria[0]['id_categoria'];
+
+            // Buscar la ID de los productos 
+
+            $stmt = $conn -> prepare("SELECT id_producto FROM producto");
+            $stmt -> execute();
+            $stmt ->setFetchMode(PDO::FETCH_ASSOC);
+            $id_productos = $stmt ->fetchAll(); 
+
+            // Preparar para insertar
+
+            $stmt = $conn -> prepare("INSERT INTO producto (id_producto, nombre, precio, id_categoria) VALUES (:id_producto,:nombre,:precio,:id_categoria)");
+            $stmt -> bindParam(':id_producto', $id_producto);
+            $stmt -> bindParam(':nombre', $nombre_producto);
+            $stmt -> bindParam(':precio', $precio_producto);
+            $stmt -> bindParam(':id_categoria', $id_categoria);
+
+            // Si está vacío
+
+            if (empty($id_productos)) {
+                $id_producto = 'P0000';
+            }
+
+            // Buscar el últim ID en caso contrario
+
+            else {
+                $id_producto = substr(end($id_productos[array_key_last($id_productos)]),1);
+                $id_producto++;
+                $id_producto =  sprintf('%04d',$id_producto);
+
+                $id_producto = 'P' . $id_producto;
+            }
+
+            $stmt -> execute();
+
+            echo 'Producto ' . $nombre_producto . ' insertado';
+        }
+
+        catch(PDOException $e) {
+            echo "Error: " . $e->getMessage();
+        }
+    }
+
+    // test_input()
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+
+?>
